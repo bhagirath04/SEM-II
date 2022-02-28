005ebcf969a04897ac9055206bc23d3a15fae6c3
bhagirath04 <95953595+bhagirath04@users.noreply.github.com>
2022-02-28 22:57:19 +0530

    Add files via upload

---
+++ C++/CheckPositive.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Eneter Integer Number: ";
+    cin>>a;
+
+    cout<<"\n";
+    
+    //logic
+    if (a>=0)
+    {
+        cout<<"Entered Number is Positive";
+    }
+    else
+    {
+        cout<<"Enetered Number is Nagative.";
+    }
+
+    
+    return 0;    
+}
