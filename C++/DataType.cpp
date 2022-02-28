533be75c1ec88244de34944e69f24c734b27ee03
bhagirath04 <95953595+bhagirath04@users.noreply.github.com>
2022-02-28 21:46:51 +0530

    Add files via upload

---
+++ C++/DataType.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    float b=2.585;
+    char c='B';
+    string s= "Bhagirath";
+    cout<<"Int a = "<<a<<endl;
+    cout<<"Float b = "<<b<<endl;
+    cout<<"Character c = "<<c<<endl;
+    cout<<"String s = "<<s<<endl;
+
+    return 0;
+}
