5ab07dd414c8cd85425b5862fc1c575c13fb739a
bhagirath04 <95953595+bhagirath04@users.noreply.github.com>
2022-02-28 22:51:43 +0530

    Add files via upload

---
+++ C++/OddEven.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Eneter Integer Number: ";
+    cin>>a;
+
+    cout<<"\n";
+    
+    //logic
+    if (a%2==0)
+    {
+        cout<<"Entered Number is Even";
+    }
+    else
+    {
+        cout<<"Enetered Number is Odd.";
+    }
+
+    
+    return 0;    
+}
