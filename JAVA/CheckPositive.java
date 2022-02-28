39543e5bd2feba6d09d0438ba93f088255a50a17
bhagirath04 <95953595+bhagirath04@users.noreply.github.com>
2022-02-28 23:05:26 +0530

    Add files via upload

---
+++ JAVA/CheckPositive.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class CheckPositive
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int a;
+        System.out.print("Eneter Integer Number: ");
+        a=sc.nextInt();
+        
+        System.out.println("\n");
+        
+        //logic
+        if (a>=0)
+        {
+                System.out.println("Enetred Number is Positive.");
+        }
+        else
+        {
+                System.out.println("Enetred Number is Nagative.");
+        }
+    }    
+}
