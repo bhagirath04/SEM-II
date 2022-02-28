cbed8c26a777f380c618d92e7f879ef19160e960
bhagirath04 <95953595+bhagirath04@users.noreply.github.com>
2022-02-28 22:25:24 +0530

    Add files via upload

---
+++ C++/VolumeOfCube.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double a,vol;
+    cout<<"Enter Length of cube A: ";
+    cin>>a;
+
+    //logic
+    vol=a*a*a;
+
+    cout<<"\n"
+    //result
+    cout<<"Volume of Cube of Length"<<a<<":"<<vol;
+
+    return 0;
+}
