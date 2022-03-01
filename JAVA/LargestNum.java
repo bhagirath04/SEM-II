41dcde5e1601f943465a7cbfd2712a19ab3ba7b4
bhagirath04 <95953595+bhagirath04@users.noreply.github.com>
2022-03-01 21:51:04 +0530

    Create LargestNum.java

---
+++ JAVA/LargestNum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class LargestNum
+{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Number 1: ");
+        a=sc.nextInt();
+        System.out.print("Enter Number 2: ");
+        b=sc.nextInt();
+        System.out.print("Enter Number 3: ");
+        c=sc.nextInt();
+
+        //logic
+        if (a>b&&a>c)
+        {
+            System.out.println("The Largest Number is "+a);
+        }
+        else if(b>a&&b>c)
+        {
+            System.out.println("The Largest Number is "+b);
+        }
+        else
+        {
+            System.out.println("The Largest Number is "+c);
+        }
+    }    
+}
